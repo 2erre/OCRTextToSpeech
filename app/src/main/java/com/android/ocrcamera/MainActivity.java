ee8266e5186472ea6b21d7f5cb2799b8ad68af80
2erre <raffaeleraggi@gmail.com>
2019-10-02 18:23:24 +0200

    first commit

---
+++ app/src/main/java/com/android/ocrcamera/MainActivity.java
@@ -0,0 +1,147 @@
+package com.android.ocrcamera;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Button;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import java.util.Locale;
+import java.lang.String;
+
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, Detector.Processor {
+
+    private SurfaceView cameraView;
+    private TextView txtView;
+    private CameraSource cameraSource;
+    private TextToSpeech tts;
+    private Button button;
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (Exception e) {
+
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cameraView = findViewById(R.id.surface_view);
+        txtView = findViewById(R.id.txtview);
+        button = findViewById(R.id.button);
+        TextRecognizer txtRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if (!txtRecognizer.isOperational()) {
+            Log.e("Main Activity", "Detector dependencies are not yet available");
+        } else {
+            cameraSource = new CameraSource.Builder(getApplicationContext(), txtRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(this);
+            txtRecognizer.setProcessor(this);
+        }
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+                if (status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.ITALIAN);
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+                return;
+            }
+            cameraSource.start(cameraView.getHolder());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        cameraSource.stop();
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections detections) {
+        SparseArray items = detections.getDetectedItems();
+        final StringBuilder strBuilder = new StringBuilder();
+
+        for (int i = 0; i < items.size(); i++) {
+            TextBlock item = (TextBlock) items.valueAt(i);
+            strBuilder.append(item.getValue());
+
+            }
+
+        Log.v("strBuilder.toString()", strBuilder.toString());
+
+        txtView.post(new Runnable() {
+            @Override
+            public void run() {
+                txtView.setText(strBuilder.toString());
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if(strBuilder.length()==0){
+                    tts.speak("Inquadra del testo per leggerlo", TextToSpeech.QUEUE_FLUSH, null);
+                }else{
+                    tts.speak(strBuilder.toString(), TextToSpeech.QUEUE_FLUSH, null);
+                }
+
+
+            }
+        });
+    }
+}
+
